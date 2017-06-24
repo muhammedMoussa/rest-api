327ccf80f5867f1f546e4e798796d7e71ececa9d
Muhammed Moussa <muhammedmoussa@hotmail.com>
2017-06-24 22:40:58 +0200

    define a remote methd on a model

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = function(Product) {
+  /**
+    * Buy this product
+    * @param {number} quantity Number of products to buy
+    * @param {Function(Error, object)} callback
+    */
+    const isQuantity = quantity => Boolean(quantity > 0)
 
+  /*
+    * Return true if input is > 0
+    * @param {number} quantity Number to validate
+    */
+
+    Product.prototype.buy = function(quantity, callback) {
+      // var result = { quantity };
+      if(!isQuantity(quantity)) {
+        return callback(`Invalid quantity ${quantity}`)
+      }
+      const result = {
+        status: `Great!, You bought ${quantity} products`
+      };
+      // TODO
+      callback(null, result);
+    };
 };
